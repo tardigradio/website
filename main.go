9280f7e38e7e75e830d7c794da0e1a8608027862
Alexander Leitner <leitnersalex@gmail.com>
2018-09-17 15:53:43 -0400

    Render templates

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+  "log"
 	"os"
 	"regexp"
+  "net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,11 +20,45 @@ func main() {
 		}
 	}
 
-  r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+  router := gin.Default()
+
+  router.LoadHTMLGlob("templates/*")
+
+  router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
 	})
-	r.Run(fmt.Sprintf(":%s", port))
+
+  router.GET("/user/:name/*song", func(c *gin.Context) {
+    name := c.Param("name")
+    song := c.Param("song")
+    c.String(http.StatusOK, "%s: %s", name, song)
+  })
+
+  router.POST("/upload", func(c *gin.Context) {
+		// single file
+		file, _ := c.FormFile("file")
+		log.Println(file.Filename)
+
+		// Upload the file to specific dst.
+		// c.SaveUploadedFile(file, dst)
+
+		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	})
+
+  router.GET("/register", func(c *gin.Context) {
+    c.String(http.StatusOK, "Register")
+  })
+
+  router.GET("/login", func(c *gin.Context) {
+    c.String(http.StatusOK, "login")
+  })
+
+  router.GET("/", func(c *gin.Context) {
+  		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+  			"title": "Tardigraud.io",
+  		})
+  	})
+
+	router.Run(fmt.Sprintf(":%s", port))
 }
