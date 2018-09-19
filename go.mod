4d6ea5efe80d9339ce19b369fe8443770d548847
Alexander Leitner <leitnersalex@gmail.com>
2018-09-19 14:25:33 -0400

    Cleanup code

--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
 module github.com/tardigraudio/website
 
 require (
+	github.com/alecthomas/gometalinter v2.0.11+incompatible // indirect
+	github.com/fatih/gomodifytags v0.0.0-20180914191908-141225bf62b6 // indirect
 	github.com/gin-contrib/sessions v0.0.0-20180827025425-58cbcf30135c
 	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/gorilla/sessions v1.1.2 // indirect
 	github.com/mattn/go-sqlite3 v1.9.0
+	github.com/mdempsky/gocode v0.0.0-20180727200127-00e7f5ac290a // indirect
+	github.com/rogpeppe/godef v1.0.0 // indirect
 	github.com/ugorji/go/codec v0.0.0-20180918125716-ed9a3b5f078b // indirect
+	golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	storj.io/storj v0.0.0-20180918152659-e1f44ec78259
 )
--- main.go
+++ main.go
@@ -21,54 +21,231 @@ import (
 	"storj.io/storj/pkg/miniogw"
 	"storj.io/storj/pkg/paths"
 	"storj.io/storj/pkg/provider"
+	"storj.io/storj/pkg/storage/buckets"
 	"storj.io/storj/pkg/storage/objects"
 	"storj.io/storj/storage"
 )
 
-func AuthRequired() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		user := session.Get("user")
-		if user == nil {
-			// You'd normally redirect to login page
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
-		} else {
-			// Continue down the chain to handler etc
-			c.Next()
-		}
-	}
+type Server struct {
+	DB *db.DB
+	r  *gin.Engine
+	bs buckets.Store
 }
 
-func main() {
-	context := context.Background()
-	port := "8080"
-
-	if len(os.Args) > 1 {
-		if matched, _ := regexp.MatchString(`^\d{2,6}$`, os.Args[1]); matched == true {
-			port = os.Args[1]
-		}
-	}
-
+func Initialize(ctx context.Context) *Server {
 	router := gin.Default()
-
 	store := cookie.NewStore([]byte("secret"))
 	router.Use(sessions.Sessions("mysession", store))
 
 	usr, err := user.Current()
 	if err != nil {
+		panic(err)
+	}
+
+	database, err := db.Open(ctx, filepath.Join(usr.HomeDir, "/.tardigraudio"))
+	if err != nil {
+		panic(err)
+	}
+
+	bs, err := getBucketStore(ctx, usr.HomeDir)
+	if err != nil {
+		panic(err)
+	}
+
+	return &Server{DB: database, r: router, bs: bs}
+}
+
+func (s *Server) Run(address string) {
+	s.r.Run(address)
+}
+
+func (s *Server) Close() error {
+	return s.DB.Close()
+}
+
+func (s *Server) GetRoot(c *gin.Context) {
+	session := sessions.Default(c)
+	var popular []string
+	var recent []string
+
+	c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		"title":       "Tardigraud.io",
+		"popular":     popular,
+		"recent":      recent,
+		"currentUser": session.Get("user"),
+	})
+}
+
+func (s *Server) PostLogin(c *gin.Context) {
+	session := sessions.Default(c)
+
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	h := sha512.New()
+	h.Write([]byte(password))
+	hash := h.Sum(nil)
+
+	user, err := s.DB.GetUser(username)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Invalid username or password")
+		return
+	}
+
+	if !bytes.Equal(hash, user.Hash) {
+		c.String(http.StatusInternalServerError, "Invalid username or password")
+		return
+	}
+
+	session.Set("user", username)
+	session.Save()
+
+	c.String(http.StatusOK, fmt.Sprintf("'%s' logged in!", username))
+}
+
+func (s *Server) GetLogin(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.tmpl", gin.H{})
+}
+
+func (s *Server) PostRegister(c *gin.Context) {
+	session := sessions.Default(c)
+	email := c.PostForm("email")
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	h := sha512.New()
+	h.Write([]byte(password))
+
+	err := s.DB.AddUser(email, username, h.Sum(nil))
+	if err != nil {
+		log.Println(err)
+		c.String(http.StatusInternalServerError, "Failed to register")
+		return
+	} else {
+		// TODO: Create bucket for user with the same name as the user sj://username
+		_, err = s.bs.Get(c, username)
+		if err == nil {
+			log.Fatal("Bucket already exists")
+		}
+		if !storage.ErrKeyNotFound.Has(err) {
+			log.Fatal(err)
+		}
+		_, err = s.bs.Put(c, username)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Bucket %s created\n", username)
+
+		session.Set("user", username)
+		session.Save()
+		c.String(http.StatusOK, fmt.Sprintf("'%s' registered!", username))
+	}
+}
+
+func (s *Server) GetRegister(c *gin.Context) {
+	c.HTML(http.StatusOK, "register.tmpl", gin.H{})
+}
+
+func (s *Server) GetLogout(c *gin.Context) {
+	session := sessions.Default(c)
+
+	session.Delete("user")
+	session.Save()
+
+	c.String(http.StatusOK, "Successfully Logged out")
+	s.r.HandleContext(c)
+}
+
+func (s *Server) GetSong(c *gin.Context) {
+	// session := sessions.Default(c)
+	username := c.Param("name")
+	song := c.Param("song")
+	c.HTML(http.StatusOK, "song.tmpl", gin.H{
+		"username": username,
+		"song":     song,
+	})
+}
+
+func (s *Server) GetUpload(c *gin.Context) {
+	session := sessions.Default(c)
+
+	c.HTML(http.StatusOK, "upload.tmpl", gin.H{
+		"currentUser": session.Get("user"),
+	})
+}
+
+func (s *Server) PostUpload(c *gin.Context) {
+	session := sessions.Default(c)
+	// single file
+	title := c.PostForm("songTitle")
+	description := c.PostForm("songDesc")
+
+	var user db.User
+	user, err := s.DB.GetUser(fmt.Sprintf("%s", session.Get("user")))
+	if err != nil {
 		log.Fatal(err)
 	}
 
+	fileHeader, _ := c.FormFile("file")
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Upload the file to STORJ
+	o, err := s.bs.GetObjectStore(c, user.Username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	meta := objects.SerializableMeta{}
+	expTime := time.Time{}
+
+	_, err = o.Put(c, paths.New(fileHeader.Filename), file, meta, expTime)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = s.DB.AddSong(title, description, user.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", title))
+	s.r.HandleContext(c)
+}
+
+func (s *Server) GetUser(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.Param("name")
+
+	var user db.User
+	user, err := s.DB.GetUser(fmt.Sprintf("%s", session.Get("user")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	uploads := []string{}
+	c.HTML(http.StatusOK, "user.tmpl", gin.H{
+		"username": username,
+		"email":    user.Email,
+		"uploads":  uploads,
+	})
+}
+
+func getBucketStore(ctx context.Context, homeDir string) (buckets.Store, error) {
 	identityCfg := provider.IdentityConfig{
-		CertPath: filepath.Join(usr.HomeDir, ".storj/uplink/identity.cert"),
-		KeyPath:  filepath.Join(usr.HomeDir, ".storj/uplink/identity.key"),
+		CertPath: filepath.Join(homeDir, ".storj/uplink/identity.cert"),
+		KeyPath:  filepath.Join(homeDir, ".storj/uplink/identity.key"),
 		Address:  ":7777",
 	}
 
 	minioCfg := miniogw.MinioConfig{
 		AccessKey: "3ee4E2vqy3myfKdPnuPKTQQavtqx",
 		SecretKey: "3H1BL6sKtiRCrs9VxCbw9xboYsXp",
-		MinioDir:  filepath.Join(usr.HomeDir, ".storj/uplink/miniogw"),
+		MinioDir:  filepath.Join(homeDir, ".storj/uplink/miniogw"),
 	}
 
 	clientCfg := miniogw.ClientConfig{
@@ -97,195 +274,55 @@ func main() {
 
 	cfg := &cmd.Config{storjCfg}
 
-	fmt.Println(cfg)
-	bs, err := cfg.BucketStore(context)
+	return cfg.BucketStore(ctx)
+}
 
-	database, err := db.Open(context, filepath.Join(usr.HomeDir, "/.tardigraudio"))
-	if err != nil {
-		panic(err)
-	}
-	defer database.Close()
-
-	router.LoadHTMLGlob("templates/*")
-	router.Static("assets/css", "assets/css")
-
-	router.GET("/user/:name", func(c *gin.Context) {
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		session := sessions.Default(c)
-		username := c.Param("name")
-
-		var user db.User
-		user, err := database.GetUser(fmt.Sprintf("%s", session.Get("user")))
-		if err != nil {
-			log.Fatal(err)
+		user := session.Get("user")
+		if user == nil {
+			// You'd normally redirect to login page
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+		} else {
+			// Continue down the chain to handler etc
+			c.Next()
 		}
+	}
+}
 
-		uploads := []string{}
-		c.HTML(http.StatusOK, "user.tmpl", gin.H{
-			"username": username,
-			"email":    user.Email,
-			"uploads":  uploads,
-		})
-	})
+func main() {
+	ctx := context.Background()
+	port := "8080"
 
-	router.GET("/user/:name/*song", func(c *gin.Context) {
-		// session := sessions.Default(c)
-		username := c.Param("name")
-		song := c.Param("song")
-		c.HTML(http.StatusOK, "song.tmpl", gin.H{
-			"username": username,
-			"song":     song,
-		})
-	})
+	if len(os.Args) > 1 {
+		if matched, _ := regexp.MatchString(`^\d{2,6}$`, os.Args[1]); matched == true {
+			port = os.Args[1]
+		}
+	}
 
-	private := router.Group("/useraction")
+	server := Initialize(ctx)
+	defer server.Close()
+
+	server.r.GET("/", server.GetRoot)
+
+	server.r.LoadHTMLGlob("templates/*")
+	server.r.Static("assets/css", "assets/css")
+
+	private := server.r.Group("/useraction")
 	private.Use(AuthRequired())
 	{
-		private.GET("/logout", func(c *gin.Context) {
-			session := sessions.Default(c)
-
-			session.Delete("user")
-			session.Save()
-
-			c.String(http.StatusOK, "Successfully Logged out")
-			router.HandleContext(c)
-		})
-
-		private.GET("/upload", func(c *gin.Context) {
-			session := sessions.Default(c)
-
-			c.HTML(http.StatusOK, "upload.tmpl", gin.H{
-				"currentUser": session.Get("user"),
-			})
-		})
-
-		private.POST("/upload", func(c *gin.Context) {
-			session := sessions.Default(c)
-			// single file
-			title := c.PostForm("songTitle")
-			description := c.PostForm("songDesc")
-
-			var user db.User
-			user, err := database.GetUser(fmt.Sprintf("%s", session.Get("user")))
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fileHeader, _ := c.FormFile("file")
-
-			file, err := fileHeader.Open()
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer file.Close()
-
-			// Upload the file to STORJ
-			o, err := bs.GetObjectStore(context, user.Username)
-			if err != nil {
-				log.Fatal(err)
-			}
-			meta := objects.SerializableMeta{}
-			expTime := time.Time{}
-
-			_, err = o.Put(context, paths.New(fileHeader.Filename), file, meta, expTime)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = database.AddSong(title, description, user.ID)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// Redirect to homepage
-			c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", title))
-			router.HandleContext(c)
-			return
-		})
+		private.GET("/logout", server.GetLogout)
+		private.GET("/upload", server.GetUpload)
+		private.POST("/upload", server.PostUpload)
 	}
 
-	router.GET("/register", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "register.tmpl", gin.H{})
-	})
+	server.r.GET("/user/:name", server.GetUser)
+	server.r.GET("/user/:name/*song", server.GetSong)
+	server.r.GET("/register", server.GetRegister)
+	server.r.POST("/register", server.PostRegister)
+	server.r.GET("/login", server.GetLogin)
+	server.r.POST("/login", server.PostLogin)
 
-	router.POST("/register", func(c *gin.Context) {
-		session := sessions.Default(c)
-		email := c.PostForm("email")
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-
-		h := sha512.New()
-		h.Write([]byte(password))
-
-		err := database.AddUser(email, username, h.Sum(nil))
-		if err != nil {
-			log.Println(err)
-			c.String(http.StatusInternalServerError, "Failed to register")
-			return
-		} else {
-			// TODO: Create bucket for user with the same name as the user sj://username
-			_, err = bs.Get(context, username)
-			if err == nil {
-				log.Fatal("Bucket already exists")
-			}
-			if !storage.ErrKeyNotFound.Has(err) {
-				log.Fatal(err)
-			}
-			_, err = bs.Put(context, username)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			fmt.Printf("Bucket %s created\n", username)
-
-			session.Set("user", username)
-			session.Save()
-			c.String(http.StatusOK, fmt.Sprintf("'%s' registered!", username))
-		}
-	})
-
-	router.GET("/login", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "login.tmpl", gin.H{})
-	})
-
-	router.POST("/login", func(c *gin.Context) {
-		session := sessions.Default(c)
-
-		username := c.PostForm("username")
-		password := c.PostForm("password")
-
-		h := sha512.New()
-		h.Write([]byte(password))
-		hash := h.Sum(nil)
-
-		user, err := database.GetUser(username)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Invalid username or password")
-			return
-		}
-
-		if !bytes.Equal(hash, user.Hash) {
-			c.String(http.StatusInternalServerError, "Invalid username or password")
-			return
-		}
-
-		session.Set("user", username)
-		session.Save()
-
-		c.String(http.StatusOK, fmt.Sprintf("'%s' logged in!", username))
-	})
-
-	router.GET("/", func(c *gin.Context) {
-		session := sessions.Default(c)
-		var popular []string
-		var recent []string
-
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":       "Tardigraud.io",
-			"popular":     popular,
-			"recent":      recent,
-			"currentUser": session.Get("user"),
-		})
-	})
-
-	router.Run(fmt.Sprintf(":%s", port))
+	server.Run(fmt.Sprintf(":%s", port))
 }
